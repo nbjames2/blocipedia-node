71dc38421343dfcfe5d7db7f872bd7d97d9a5486
Nick <nbjames2@gmail.com>
2019-01-07 21:13:20 -0500

    Scaffolding checkpoint complete

---
+++ src/config/main-config.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const session = require("express-session");
+const flash = require("express-flash");
+// const passportConfig = require("./passport-config");
+const logger = require('morgan');
+
+module.exports = {
+    init(app, express){
+        app.set("views", viewsFolder);
+        app.set("view engine", "ejs");
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(expressValidator());
+        app.use(session({
+            secret: process.env.cookieSecret,
+            resave: false,
+            saveUninitialized: false,
+            cookie: { maxAge: 1.21e+9 }
+        }));
+        app.use(flash());
+        app.use(express.static(path.join(__dirname, "..", "assets")));
+        // passportConfig.init(app);
+        app.use(logger('dev'));
+    }
+}
