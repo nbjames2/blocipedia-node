ba2077d22b5fa593fc113f5ef48054b335a6cda4
Nick <nbjames2@gmail.com>
2019-01-08 11:43:16 -0500

    scaffolding 3

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,7 +1,26 @@
 require("dotenv").config();
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const session = require("express-session");
+const flash = require("express-flash");
  
 module.exports = {
- init(){
+ init(app, express){
+  app.set("views", viewsFolder);
+  app.set("view engine", "ejs");
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(expressValidator());
+  app.use(session({
+      secret: process.env.cookieSecret,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 1.21e+9 }
+  }));
+  app.use(flash());
+  app.use(express.static(path.join(__dirname, "..", "assets")));
+  app.use(expressValidator());
 
   }
 }; 
